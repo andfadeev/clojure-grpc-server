560a8535f81e18502e463c7351c0ea2372803d2d
Andrey Fadeev <andfadeev@gmail.com>
2024-05-30 14:15:02 +0100

    add_service

--- src/clojure/clojure_grpc_server/core.clj
+++ src/clojure/clojure_grpc_server/core.clj
@@ -1,18 +1,19 @@
 (ns clojure-grpc-server.core
   (:gen-class)
   (:require [clojure.string :as str])
-  (:import (com.kroo.api.v1 HelloRequest HelloResponse HelloServiceGrpc HelloServiceGrpc$HelloServiceImplBase)
+  (:import (com.kroo.api.v1 AddRequest AddResponse AddServiceGrpc AddServiceGrpc$AddServiceImplBase HelloRequest HelloResponse HelloServiceGrpc HelloServiceGrpc$HelloServiceImplBase)
            (io.grpc ManagedChannelBuilder Server ServerBuilder)
            (io.grpc.stub StreamObserver)))
 
-;; README:
+;; README
+
 ;; Source of truth for API is proto folder, a gRPC service is defined in `hello_service.proto`
 ;; By running `buf generate` it will use Protobuf compiler to generate Java classes (or other languages if specified)
 ;; `buf lint` could be used to detect naming issues, etc.
 
 ;; The most important feature is to detect breaking changes with `buf breaking ...`, more about options: https://buf.build/docs/reference/inputs
 
-;; All those buf cli steps should be part of checks in CI, so we protected against breaking changes merges.
+;; All those Buf CLI steps should be part of checks in CI, so we protected against breaking changes merges.
 ;; If API is under development there is a concept unstable API that allow all changes.
 
 ;; Other benefit is that same code will be used in clients for gRPC requests, but also could be used in tests.
@@ -38,22 +39,46 @@
     (let [response (.hello stub request)]
       (println "Got response, the greeting:" (.getGreeting response)))))
 
+(defn call-grpc-add
+  []
+  (let [channel (-> (ManagedChannelBuilder/forAddress "localhost" 9001)
+                    (.usePlaintext)
+                    (.build))
+        stub (AddServiceGrpc/newBlockingStub channel)
+        ;; Build request object (Buf generated class from .proto schemas)
+        request (->(AddRequest/newBuilder)
+                   (.setX 1)
+                   (.setY 2)
+                   (.build))]
+    (println "Calling hello RPC")
+    ;; Execute gRPC
+    (let [response (.add stub request)]
+      (println "Sum of 1 and 2 is" (.getSum response)))))
+
 ;; gRPC Server implementation: https://github.com/otwieracz/clj-grpc/blob/master/src/clj/clj_grpc/server.clj
+
+(def ^HelloServiceGrpc add-service-impl
+  (proxy
+    [AddServiceGrpc$AddServiceImplBase]
+    []
+    (add [ ^AddRequest request ^StreamObserver response-observer]
+      (println "Handling request /add in gRPC server" request)
+      (let [response (-> (AddResponse/newBuilder)
+                         (.setSum (+ (.getX request) (.getY request)))
+                         (.build))]
+        (.onNext response-observer response)
+        (.onCompleted response-observer)))))
 
 (def ^HelloServiceGrpc hello-service-impl
   (proxy
     [HelloServiceGrpc$HelloServiceImplBase]
     []
-
     (hello [ ^HelloRequest request ^StreamObserver response-observer]
-
+      (call-grpc-add)
       ;; This is where we can start writing plain Clojure code
       ;; we can get all data from Buf generated classes for request, e.g. HelloRequest
-
       (println "Handling request /hello in gRPC server" request)
       (let [greeting (str/join " " ["Hello" (.getFirstName request) (.getLastName request)])
-
-
             ;; Finally we need to convert Clojure data to generated response class, e.g. HelloResponse
             response (-> (HelloResponse/newBuilder)
                          (.setGreeting greeting)
@@ -63,10 +88,10 @@
 
 (defn create-grpc-server
   []
-
   (let [^Server grpc-server (-> (ServerBuilder/forPort 9001)
                                 ;; Multiple gRPC services could be added here, allowing modular code
                                 (.addService hello-service-impl)
+                                (.addService add-service-impl)
                                 (.build))]
     (.start grpc-server)
     (.awaitTermination grpc-server)))
@@ -76,8 +101,6 @@
   (future
     (Thread/sleep 5000)
     (call-grpc-hello))
-
   (println "Creating gRPC server")
   (create-grpc-server))
-
 
