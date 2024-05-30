ffb4eab9f293ff0eef6c81357e014b6c7df9da6a
Andrey Fadeev <andfadeev@gmail.com>
2024-05-30 16:56:36 +0100

    try closable pattern

--- src/clojure/clojure_grpc_server/core.clj
+++ src/clojure/clojure_grpc_server/core.clj
@@ -1,9 +1,11 @@
 (ns clojure-grpc-server.core
   (:gen-class)
   (:require [clojure.string :as str])
-  (:import (com.kroo.api.v1 AddRequest AddResponse AddServiceGrpc AddServiceGrpc$AddServiceImplBase HelloRequest HelloResponse HelloServiceGrpc HelloServiceGrpc$HelloServiceImplBase)
-           (io.grpc BindableService ManagedChannelBuilder Server ServerBuilder)
-           (io.grpc.stub StreamObserver)))
+  (:import (clojure.lang IDeref)
+           (com.kroo.api.v1 AddRequest AddResponse AddServiceGrpc AddServiceGrpc$AddServiceImplBase HelloRequest HelloResponse HelloServiceGrpc HelloServiceGrpc$HelloServiceImplBase)
+           (io.grpc BindableService ManagedChannel ManagedChannelBuilder Server ServerBuilder)
+           (io.grpc.stub StreamObserver)
+           (java.io Closeable)))
 
 ;; README
 
@@ -22,13 +24,8 @@
 ;; Try by running `lein run`, it will start gRPC server on 9001 and do a delayed call to Hello RPC
 
 (defn call-grpc-hello
-  []
-  (println "Creating gRPC channel")
-  ;; Channel should be shared
-  (let [channel (-> (ManagedChannelBuilder/forAddress "localhost" 9001)
-                    (.usePlaintext)
-                    (.build))
-        stub (HelloServiceGrpc/newBlockingStub channel)
+  [{:keys [grpc-channel]}]
+  (let [stub (HelloServiceGrpc/newBlockingStub grpc-channel)
         ;; Build request object (Buf generated class from .proto schemas)
         request (->(HelloRequest/newBuilder)
                    (.setFirstName "Andrey")
@@ -39,12 +36,15 @@
     (let [response (.hello stub request)]
       (println "Got response, the greeting:" (.getGreeting response)))))
 
+(defn create-grpc-channel
+  [{:keys [host port]}]
+  (-> (ManagedChannelBuilder/forAddress host port)
+      (.usePlaintext)
+      (.build)))
+
 (defn call-grpc-add
-  []
-  (let [channel (-> (ManagedChannelBuilder/forAddress "localhost" 9001)
-                    (.usePlaintext)
-                    (.build))
-        stub (AddServiceGrpc/newBlockingStub channel)
+  [{:keys [grpc-channel]}]
+  (let [stub (AddServiceGrpc/newBlockingStub grpc-channel)
         ;; Build request object (Buf generated class from .proto schemas)
         request (->(AddRequest/newBuilder)
                    (.setX 1)
@@ -57,7 +57,8 @@
 
 ;; gRPC Server implementation: https://github.com/otwieracz/clj-grpc/blob/master/src/clj/clj_grpc/server.clj
 
-(def ^BindableService add-service-impl
+(defn ^BindableService build-add-service
+  [{:keys [grpc-channel]}]
   (proxy
     [AddServiceGrpc$AddServiceImplBase]
     []
@@ -69,12 +70,13 @@
         (.onNext response-observer response)
         (.onCompleted response-observer)))))
 
-(def ^BindableService hello-service-impl
+(defn ^BindableService build-hello-service
+  [{:keys [grpc-channel] :as deps}]
   (proxy
     [HelloServiceGrpc$HelloServiceImplBase]
     []
     (hello [^HelloRequest request ^StreamObserver response-observer]
-      (call-grpc-add)
+      (call-grpc-add deps)
       ;; This is where we can start writing plain Clojure code
       ;; we can get all data from Buf generated classes for request, e.g. HelloRequest
       (println "Handling request /hello in gRPC server" request)
@@ -87,20 +89,50 @@
         (.onCompleted response-observer)))))
 
 (defn create-grpc-server
-  []
+  [{:keys [hello-service
+           add-service]}]
   (let [^Server grpc-server (-> (ServerBuilder/forPort 9001)
                                 ;; Multiple gRPC services could be added here, allowing modular code
-                                (.addService hello-service-impl)
-                                (.addService add-service-impl)
+                                (.addService hello-service)
+                                (.addService add-service)
                                 (.build))]
     (.start grpc-server)
-    (.awaitTermination grpc-server)))
+    grpc-server))
+
+(defn closeable
+  ([value] (closeable value identity))
+  ([value close] (reify
+                   IDeref
+                   (deref [_] value)
+                   Closeable
+                   (close [_] (close value)))))
+
+(defn run-system
+  []
+  (with-open [grpc-channel (closeable
+                             (create-grpc-channel {:host "localhost"
+                                                   :port 9001})
+                             (fn [^ManagedChannel channel]
+                               (.shutdown channel)))
+              hello-service (closeable
+                              (build-hello-service {:grpc-channel @grpc-channel}))
+              add-service (closeable
+                            (build-add-service {:grpc-channel @grpc-channel}))
+              grpc-server (closeable
+                            (create-grpc-server
+                              {:hello-service @hello-service
+                               :add-service @add-service})
+                            (fn [grpc-server]
+                              (.shutdown grpc-server)))]
+    (println "System started")
+
+    ;; Delay call to the gRPC server
+    (future
+      (Thread/sleep 5000)
+      (call-grpc-hello {:grpc-channel @grpc-channel}))
+
+    (.awaitTermination @grpc-server)))
 
 (defn -main
   []
-  (future
-    (Thread/sleep 5000)
-    (call-grpc-hello))
-  (println "Creating gRPC server")
-  (create-grpc-server))
-
+  (run-system))
